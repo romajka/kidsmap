df086574d72a1d4c5c7510c7fa6ea812010a13d1
Ramin Cavadov <1zic.rs1@gmail.com>
2026-02-23 12:32:11 +0400

    add place detail page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import place_list
+from .views import place_list, place_detail
 
 urlpatterns = [
     path("", place_list, name="place_list"),
+    path("place/<int:pk>/", place_detail, name="place_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Place
 
 def place_list(request):
     places = Place.objects.all()
     return render(request, "catalog/place_list.html", {"places": places})
+
+def place_detail(request, pk: int):
+    place = get_object_or_404(Place, pk=pk)
+    return render(request, "catalog/place_detail.html", {"place": place})
